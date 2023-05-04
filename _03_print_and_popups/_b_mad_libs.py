44065dfffbdd105629dc21247330e3fcecd7e73f
root <elizabethanne317@gmail.com>
2023-05-04 02:52:43 +0000

    i worked on more tk stuff

--- _03_print_and_popups/_b_mad_libs.py
+++ _03_print_and_popups/_b_mad_libs.py
@@ -9,17 +9,20 @@ if __name__ == '__main__':
     # Hide the window using the window's .withdraw() method
 
     # Put this sentence in a pop-up message box:
-    ('If you find yourself having to crss a pranha-infested river, here is how to do it...')
+    simpledialog.askstring(title='Greeter', prompt="If you find yourself having to cross a piranha-infested river, "
+                                                   "here is how to do it...\n"
+                                          "are you ready???")
+
     # Get the player to enter an adjective
-
+    adj = simpledialog.askstring(title='adj', prompt="enter an adjective")
     # Get the player to enter a type of liquid
-
+    tol = simpledialog.askstring(title='type of liquid', prompt="enter a type of liquid")
     # Get the player to enter a body part
-
+    bp = simpledialog.askstring(title='body part', prompt="enter a part of your body")
     # Get the player to enter a verb
-
+    v = simpledialog.askstring(title='verb', prompt="enter a verb")
     # Get the player to enter a place
-
+    p = simpledialog.askstring(title='place', prompt="enter a place")
     # The story below has has been written as a group of Strings joined
     # together by + signs. The story contains place holders, indicated
     # by [** **] which you need to replace with the values entered by the
@@ -28,20 +31,21 @@ if __name__ == '__main__':
     #       other parts of the story.
 
     story = (
-        "Piranhas are more [**adjective**] during the day, so cross the river at\n"
-        "night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"
-        "likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"
-        "you do, if you have an open wound, try to find another way to get "
-        "back to the [**place**]. Good luck!"
+        "Piranhas are more " + adj + " during the day, so cross the river at\n"
+        "night. Piranhas are attracted to fresh " + tol + " and will most\n"
+        "likely take a bite out of your " + bp + " if you " + v + ". Whatever\n"
+        "you do, if you have an open wound, try to find another way\n"
+        "to get back to the " + p + ". Good luck!"
     )
 
     # Make a pop-up that contains the final story. The \n escape characters add
     # line breaks to the story. If you need to, move them around to make your
     # story look better in the pop-up
+    simpledialog.askstring(title="cool thing", prompt=story)
 
     # If you want to write your own Madlib story, just change the story variable
     # and ask the player different questions.
-
+    window.mainloop()
     # Run the window's .mainloop() method
 
     pass
--- _03_print_and_popups/_c_world_domination.py
+++ _03_print_and_popups/_c_world_domination.py
@@ -1,15 +1,24 @@
 from tkinter import messagebox, simpledialog, Tk
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
-    # Make a new window variable, window = Tk()
-    
-    # Hide the window using the window's .withdraw() method
-    
-    # 1. Ask the user if they know how to write code.
-    
-    # 2. If they say "yes", tell them they will rule the world in a message box pop-up.
+if __name__ == '__main__':
+    window = Tk()
+    window.withdraw()
+# Make a new window variable, window = Tk()
 
-    # 3. Otherwise, tell them to sign up for classes at The League in an error box pop-up.
-    
-    # Run the window's .mainloop() method
+# Hide the window using the window's .withdraw() method
+
+# 1. Ask the user if they know how to write code.
+s = simpledialog.askstring(title="code", prompt="do you know how to write code?")
+# 2. If they say "yes", tell them they will rule the world in a message box pop-up.
+if s == "yes":
+    messagebox.Message("You will rule the world!")
+    messagebox.showinfo()
+else:
+    messagebox.Message("sign up for a coding class!")
+    messagebox.showinfo()
+
+# 3. Otherwise, tell them to sign up for classes at The League in an error box pop-up.
+
+# Run the window's .mainloop() method
+    window.mainloop()
