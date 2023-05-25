deeed47148805aa6365c09a8ac35a806d0361318
root <elizabethanne317@gmail.com>
2023-05-25 03:00:07 +0000

    i did more coding and stuff and learned some cool new things

--- _03_print_and_popups/_e_awesome_or_not.py
+++ _03_print_and_popups/_e_awesome_or_not.py
@@ -9,11 +9,10 @@ if __name__ == '__main__':
 # Hide the window using the window's .withdraw() method
 
 # 1. Make a variable equal to a positive number less than 4 using random.randInt(0, 3)
-n = random.randInt(0, 3)
+n = random.randint(0, 3)
 # 2. Print your variable to the console
-print(n)
 # 3. Get the user to enter something that they think is awesome
-
+input("Enter something that you think is awesome:")
 # 4. If your variable is  0
 # -- tell the user whatever they entered is awesome!
 
@@ -25,5 +24,13 @@ print(n)
 
 # 7. If your variable is  3
 # -- invent your own message to give to the user (be nice).
-
+if n == 0:
+    print("What you entered is awesome!")
+if n == 1:
+    print("What you entered is ok i guess ")
+if n == 2:
+    print("what you entered is boring.")
+if n == 3:
+    print("please. anything but that. what you entered is terrible.")
 # Run the window's .mainloop() method
+window.mainloop()
--- _04_amazing_games/_b_high_low_game.py
+++ _04_amazing_games/_b_high_low_game.py
@@ -7,22 +7,32 @@ if __name__ == '__main__':
     window.withdraw()
 
     # 1. Change this line to give you a random number between 1 - 100.
-    random_num = random.randint(1, 10)
+n = random.randint(1, 100)
 
-    # 2. Print out the random variable that you made in step #1
+# 2. Print out the random variable that you made in step #1
 
-    # 3. Code a for loop to run steps 4-10, 10 times
+# 3. Code a for loop to run steps 4-10, 10 times
+for i in range(1):
+    guess = simpledialog.askinteger(title="idk", prompt="guess a number between 1 and 100")
+    if guess > n:
+        print("too high, you lost :C")
+    else:
+        if guess < n:
+            print("too low, you lost :C")
+        else:
+            if guess == n:
+                print("you won! :D")
+                #sys.exit(0)
+# 4. Ask the user for a guess using a pop-up window, and save their response
 
-        # 4. Ask the user for a guess using a pop-up window, and save their response
+# 5. If the guess is correct
+# 6. Win. Use 'sys.exit(0)' to end the program
 
-        # 5. If the guess is correct
-            # 6. Win. Use 'sys.exit(0)' to end the program
+# 7. if the guess is high
+# 8. Tell them it's too high
+# 9. Else if the guess is low
+# 10. Tell them it's too low
 
-        # 7. if the guess is high
-            # 8. Tell them it's too high
-        # 9. Else if the guess is low
-            # 10. Tell them it's too low
+# 11. Outside of the loop, tell the user they lost
 
-    #11. Outside of the loop, tell the user they lost
-
-    window.mainloop()
+window.mainloop()
